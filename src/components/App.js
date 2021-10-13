fa16b3aef2bfa8933e20bc1f2d5d41f7899d7c46
lgfh98 <lgfigueroah@outlook.com>
2021-10-13 11:27:47 -0500

    promesas+lotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+const useData = (URL) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    axios
+      .get(URL)
+      .then(({ data }) => {
+        ReactDOM.unstable_batchedUpdates(() => {
+          setData(data);
+          setLoading(false);
+        });
+      })
+      .catch((error) => {
+        ReactDOM.unstable_batchedUpdates(() => {
+          setError(error);
+          setLoading(false);
+        });
+      });
+  }, [URL]);
+
+  return { data, loading, error };
+};
 
 const App = () => {
-  const usersData = [
-    {
-      name: "Rodolfo",
-      email: "Rodolfo@platzi.com",
-      personalSite: "Rodolfo.com",
-    },
-    {
-      name: "Rodolfo",
-      email: "Rodolfo@platzi.com",
-      personalSite: "Rodolfo.com",
-    },
-  ];
-  const [users, setUsers] = useState(usersData);
+  const {
+    data: users,
+    loading,
+    error,
+  } = useData("https://jsonplaceholder.typicode.com/users");
+
+  console.log("render");
 
   const ponerFilas = () =>
-    users.map(({ name, email, personalSite }) => (
-      <tr>
+    users.map(({ id, name, email, website }) => (
+      <tr key={id}>
         <td>{name}</td>
         <td>{email}</td>
-        <td>{personalSite}</td>
+        <td>{website}</td>
       </tr>
     ));
+
+  if (loading) {
+    return "Cargando...";
+  }
+
+  if (error) {
+    return "Error ❌";
+  }
 
   return (
     <div className="margen">
